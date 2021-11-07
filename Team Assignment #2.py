74ab43e2737517bc914d5aadfe20a0cf709e7e25
CSIS 225 <92814687+csisteamassigments@users.noreply.github.com>
2021-11-07 16:15:01 -0600

    Update Team Assignment #2.py

--- Team Assignment #2.py
+++ Team Assignment #2.py
@@ -1,6 +1,18 @@
-#Jose Contreras
+#Jose Contreras & Pedro Rodriguez
 # Date 11/2/21
-#
+#import module 
+#methord to generate random number between 1-10 
+#print Please guessa number between 1-10
+#Taking inputs 
+#input if number is less than dice throw 
+#print please guess higher
+#print guess another number 
+#input elif number is greater than dice throw 
+#print plese guess lower 
+#print guess another number
+#if guess is correct break
+#print Congratulations 
+
 import random
 diceThrow = random.randrange(1,11)
 number = int(input("please guess a number between 1-10"))
